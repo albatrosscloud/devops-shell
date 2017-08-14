918de01a6bbe8eae84798ab53dfee783e5a19d36
José Luis EA <jose@albatross.pe>
2017-08-14 16:53:23 -0500

    Update install-tools.sh

--- bash/install-tools.sh
+++ bash/install-tools.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/albatrosscloud/wait-for-it.git
 
-ln -s listen.sh /bin/listen
+ln -s /lost+found/devops-shell/bash/listen.sh /bin/listen
 ln -s /lost+found/devops-shell/bash/wait-for-it/wait-for-it.sh /bin/wait-for-it
